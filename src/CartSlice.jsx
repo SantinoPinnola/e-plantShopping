fd0525db0edc5804b8da10cefabc73649f0c672e
santino <santinopinnola9@gmail.com>
2024-09-10 12:31:22 -0400

    committt

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,19 +1,29 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 export const CartSlice = createSlice({
-  name: 'cart',
+  name: "cart",
   initialState: {
     items: [], // Initialize items as an empty array
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const { name, image, cost } = action.payload;
+      const itemExists = state.items.find((item) => item.name === name);
+      if (itemExists) {
+        itemExists++;
+      } else {
+        state.items.push({ name, image, cost, quantity: 1 });
+      }
     },
     removeItem: (state, action) => {
+      state.items = state.items.filter((item) => item.name !== action.payload);
     },
     updateQuantity: (state, action) => {
-
-    
+      const { name, quantity } = action.payload;
+      const itemUpdate = state.items.find((item) => item.name === name);
+      if (itemUpdate) {
+        itemUpdate.quantity = quantity;
+      }
     },
   },
 });
